8714f8b8102f16e64750844ff5fbfbe8240b2c31
davidgbarron <dgbarron@live.com>
2025-09-09 20:05:43 -0400

    added random movie picker

---
+++ RandomPicker.py
@@ -0,0 +1,12 @@
+movies = ["Spirited away", "Howl's moving castle", "Princess mononoke", "From up on poppy hill", "Arietty", "Ponyo", "The wind rises", "The boy and the heron", "Castlel in the sky",
+          "Kiki's Delivery service", "When marnie was there", "Totoro", "The cat returns", "Tales form earthsea", "Grave of the fireflies", "Nausicaa and the valley of the wind",
+          "Princess Kaguya", "Pom Poko", "Ocean waves", "Whisper of the heart"]
+
+import random
+
+
+r = random.randint(0,len(movies))
+
+
+print(movies[r], ": will be the movie to watch!!")
+
